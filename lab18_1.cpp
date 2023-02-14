39148f973c1eaf7af41016a522cca62b296879d7
cpi2544 <pokpong1789@gmail.com>
2023-02-14 15:54:38 +0700

    lab18.cpp completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -3,37 +3,37 @@ using namespace std;
 
 int main(){
 	int nA,nB;
-	int *A,*B,*C;	
-	
+	int *A,*B;	
+
 	cout << "Length of A: ";
 	cin >> nA;
-	
+
 	A = new int;
 	cout << "Input Array A: ";
 	for(int i=0; i < nA; i++) cin >> A[i];
-	
+
 	cout << "Length of B: ";
 	cin >> nB;
-	
+
 	B = new int;
 	cout << "Input Array B: ";
 	for(int i=0; i < nB; i++) cin >> B[i];
-	
-	C = new int;
-	for(int i=0; i < nA; i++) C[i] = new int;
-	
+
+	int **C = new int*[nA];
+	for(int i=0; i < nA; i++) C[i] = new int[nB];
+
 	for(int i=0; i < nA; i++){
 		for(int j=0; j < nB; j++) {
-			C = A[i]*B[j];
+			C[i][j] = A[i]*B[j];
 		}
 	}
-	
+
 	cout << "\t";
 	for(int i=0; i < nB; i++){
 		cout << B[i] << "\t";
 	}
 	cout << "\n-----------------------------\n";
-	
+
 	for(int i=0; i < nA; i++){
 		cout << A[i] << "\t";
 		for(int j=0; j < nB; j++) {
@@ -41,7 +41,11 @@ int main(){
 		}
 		cout << "\n";
 	}
-     delete A,B,C; 
+
+   delete []A;
+	 delete []B;
+	 for(int i=0; i<nA ;i++) delete [] C[i];
+	 delete [] C; 
 
 	return 0;
 }
